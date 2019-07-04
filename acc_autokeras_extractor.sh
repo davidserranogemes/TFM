16871d3c693c43a98f6f678dfcfae4243d643c85
davidserranogemes <davidserranogemes@hotmail.com>
2019-07-04 14:24:02 +0200

    Sustituido acc_extractor por acc_autokeras_extractor. En previosion de los nuevos algoritmos

---
+++ acc_autokeras_extractor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd
+cd master/TFM/
+
+
+VALUE="acc_$1"
+
+#Dont collapse same values
+#cat -A logs/$1.txt | grep "Epoch"  | tr -dc '0-9.: ' | tr -s " " "\n" | grep :$ | tr ":" " "
+
+#Collapse same values
+cat -A $1 | grep "Epoch" | tr -dc '0-9.:\ Epoch' | tr -s " " "\n" | grep ":$\|Epoch" |tr ":" "\t " | sed 's/^ch\(.*\)/\1/' | tr "\n" " " | tr "\t" "\n" | sed 's/^ \(.*\)/\1/' | uniq > $VALUE
+
+
+
