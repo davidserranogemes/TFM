408add2c7d0f0d32ce69cbd1f7a5b24638fdc10a
davidserranogemes <davidserranogemes@hotmail.com>
2019-08-07 18:34:55 +0200

    Mejorada la extraccion de las metricas en h2o. Ahora recogen todos los modelos generados en orden de creacion

--- res_h2o_model_extractor.sh
+++ res_h2o_model_extractor.sh
@@ -6,4 +6,14 @@ cd master/TFM/
 VALUE="$1_mean_class_error.txt"
 
 
-cat -A $1 | grep -e "DL_random_grid" | grep "^[0-9]" | awk '{print $4 $5}' | sed 's/\(.*\)\([0-9]\.[0-9]*\)/\1 \2/' > $VALUE
+#cat -A $1 | grep -e "DL_random_grid" | grep "^[0-9]" | awk '{print $4 $5}' | sed 's/\(.*\)\([0-9]\.[0-9]*\)/\1 \2/' > $VALUE
+echo $1
+
+TEST=$(cat -A $1 | grep "AUC" | wc -l)
+
+if [ $TEST -gt 0 ]
+then
+	cat  $1 | grep "AUC"  > $VALUE
+else
+	cat  $1 | grep "Mean Per-Class Error:" > $VALUE
+fi
--- res_h2o_model_log_extractor.sh
+++ res_h2o_model_log_extractor.sh
@@ -6,4 +6,7 @@ cd master/TFM/
 VALUE="$1_mean_class_error.txt"
 
 
-cat -A $1 | grep "New leader" | tr -dc '0-9. \n' | awk '{print $3}' > $VALUE
+#cat -A $1 | grep "New leader" | tr -dc '0-9. \n' | awk '{print $3}' > $VALUE
+
+cat $1 | grep "^DeepLearning_grid" | awk '{print $1 $2}' | tr '_' ' ' | awk '{print $8}' | sed 's/\([0-9]\?[0-9]\)\(0\.[0-9]\+\)/\1 \2/' | sort -n >$VALUE
+
