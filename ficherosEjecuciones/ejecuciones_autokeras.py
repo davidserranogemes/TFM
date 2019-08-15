51e296783c247273b94646a11a36396e7172276d
davidserranogemes <davidserranogemes@hotmail.com>
2019-08-15 12:29:39 +0200

    Al final nada cambia

--- ficherosEjecuciones/ejecuciones_autokeras.py
+++ ficherosEjecuciones/ejecuciones_autokeras.py
@@ -26,23 +26,17 @@ from sklearn.model_selection import StratifiedKFold
 
 def load_letters():
 	seed = 7
-
 	dirname = 'letters.csv'
 	url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/letter-recognition/letter-recognition.data'
 	local_filename, headers = urllib.request.urlretrieve(url, dirname)  
-
 	n_cols= 17
 	n_rows=20000
-
 	X = np.genfromtxt(local_filename, delimiter= ',',usecols = range(1,n_cols))
 	y = np.genfromtxt(local_filename, delimiter= ',',usecols = 0, dtype=None)
-
-
 	skf = StratifiedKFold(n_splits=2, random_state = seed)
 	for train_index, test_index in skf.split(X, y):
 		X_train, X_test = X[train_index], X[test_index]
 		y_train, y_test = y[train_index], y[test_index]
-
 	return (X_train, y_train), (X_test, y_test)
 
 
@@ -62,22 +56,15 @@ def load_datasets(name):
 		num_words=10000
 		skip_top= 20
 		(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=num_words,skip_top =skip_top)
-
 		X_train = np.zeros((len(x_train),num_words))
 		X_test= np.zeros((len(x_test),num_words))
-
 		for i in range(1,len(x_train)):
 			X_train[i,np.unique(x_train[i])[1:]] = True
 		for i in range(1,len(x_train)):
 			X_test[i,np.unique(x_test[i])[1:]] = True
-
 		return (X_train, y_train), (X_test, y_test) 
-
-
-
 	if name=="letters":
 		return	load_letters()
-
 	sys.exit("Wrong dataset name.")
 
 
@@ -105,7 +92,7 @@ if __name__=='__main__':
 
 
 
-	time_limit = 12*60*60
+	time_limit = 1*6*60
 	
 
 	#Lectura de datos
@@ -140,39 +127,39 @@ if __name__=='__main__':
 		
 	else:
 		if mode=="Feedforward":
-			print("Executing ", datasets, "with ", mode, " arquitecture.\n")
+print("Executing ", datasets, "with ", mode, " arquitecture.\n")
 
 
 
-			x_train = np.squeeze(x_train.reshape((x_train.shape[0], -1)))
-			x_test = np.squeeze(x_test.reshape((x_test.shape[0], -1)))
-			y_train, y_encoder = transform_y(y_train)
-			y_test, _ = transform_y(y_test)
+x_train = np.squeeze(x_train.reshape((x_train.shape[0], -1)))
+x_test = np.squeeze(x_test.reshape((x_test.shape[0], -1)))
+y_train, y_encoder = transform_y(y_train)
+y_test, _ = transform_y(y_test)
 
-			start_time = time.time()
-			mlpModule = MlpModule(loss=classification_loss, metric=Accuracy, searcher_args={}, verbose=True)
-			# specify the fit args
-			data_transformer = DataTransformerMlp(x_train)
-			train_data = data_transformer.transform_train(x_train, y_train)
-			test_data = data_transformer.transform_test(x_test, y_test)
-			fit_args = {
-				"n_output_node": y_encoder.n_classes,
-				"input_shape": x_train.shape,
-				"train_data": train_data,
-				"test_data": test_data
-				}
-			mlpModule.fit(n_output_node=fit_args.get("n_output_node"),
-				input_shape=fit_args.get("input_shape"),
-				train_data=fit_args.get("train_data"),
-				test_data=fit_args.get("test_data"),
-				time_limit=time_limit)
+start_time = time.time()
+mlpModule = MlpModule(loss=classification_loss, metric=Accuracy, searcher_args={}, verbose=True)
+# specify the fit args
+data_transformer = DataTransformerMlp(x_train)
+train_data = data_transformer.transform_train(x_train, y_train)
+test_data = data_transformer.transform_test(x_test, y_test)
+fit_args = {
+	"n_output_node": y_encoder.n_classes,
+	"input_shape": x_train.shape,
+	"train_data": train_data,
+	"test_data": test_data
+	}
+mlpModule.fit(n_output_node=fit_args.get("n_output_node"),
+	input_shape=fit_args.get("input_shape"),
+	train_data=fit_args.get("train_data"),
+	test_data=fit_args.get("test_data"),
+	time_limit=time_limit)
 
 
-			#mlpModule.export_autokeras_model(datasets+"_"+mode+"_"+arquitecture+"_"+modified)
-			#y_hat = mlpModule.predict(x_test)
+#mlpModule.export_autokeras_model(datasets+"_"+mode+"_"+arquitecture+"_"+modified)
+#y_hat = mlpModule.predict(x_test)
 
-			#print("Accuracy final:", np.sum(y_test==y_hat)/len(y_hat)*100)
-			print("Time consumed: ",time_limit/3600," hours")
+#print("Accuracy final:", np.sum(y_test==y_hat)/len(y_hat)*100)
+print("Time consumed: ",time_limit/3600," hours")
 
 
 
